87f56abf4769a8569770795c61b225bc7e0dc419
Bernhard <berni98k@gmail.com>
2018-03-22 18:46:16 +0100

    append csv if it already exits

--- get-citybike-rides.py
+++ get-citybike-rides.py
@@ -3,12 +3,25 @@ from bs4 import BeautifulSoup
 import csv
 import getpass
 import umlaut
+import os.path
+from datetime import datetime
+
+outputfile = 'rides.csv'
+last_existing_time = datetime.min # saves the time of the last known ride
 
 # get the login data from the user
 login_data = {}
 login_data["username"] = raw_input("Username: ")
 login_data["password"] = getpass.getpass("Password: ")
 
+# look if a csv file already exists so only new rides will be loaded
+# if there are any errors (not existing, wrong content in column, etc) just assume the file needs to be (re)created
+try:
+    with open(outputfile, 'r') as f:
+        last_existing_time = datetime.strptime(list(csv.reader(f))[-1][3], '%d.%m.%Y %H:%M')
+except:
+    print("Error in reading existing file. It will be created or overwritten.")
+    
 # start a request session to store the login cookie
 s = requests.Session()
 
@@ -42,8 +55,12 @@ line_num = int(tab.get_text().split(' ')[0])
 print(str(line_num) + " rides found")
 
 
+newdata = True #helper for aborting the double loop
 # load all pages and add them to the outputs
 for i in range(0, line_num, 5):
+    if(not newdata): #check if the inner loop was aborted
+        break
+    
     # load the current table
     print("Loading page " + str(i/5+1) + "/" + str(line_num/5+1))
     data_url = "https://www.citybikewien.at/de/meine-fahrten?start=" + str(i)
@@ -72,12 +89,26 @@ for i in range(0, line_num, 5):
         # remove newlines and replace umlaute
         output_row = [umlaut.normalize(t.replace('\n', ' ').strip()) for t in output_row]
 
-        # add the row to the output array
-        output.append(output_row)
+        # check if the row is newer then the last ride from the csv
+        time = datetime.strptime(output_row[3], '%d.%m.%Y %H:%M')
+        if(time > last_existing_time):
+            # add the row to the output array
+            output.append(output_row)
+        else:
+            # stop the datacollection if the ride already exists
+            print("All new data loaded. Abort data collection")
+            newdata = False
+            break
 
-# write the output array to the csv and add headers
+# reverse the output array so the newest rides come last
+output.reverse()
+
+# write the output array to the csv
 print("writing csv")
-with open('rides.csv', 'wb') as f:
+with open(outputfile, 'ab') as f:
     writer = csv.writer(f)
-    writer.writerow(['date', 'id', 'start_station', 'start_time', 'end_station', 'end_time', 'price'])
+    # if it is a new file or has an error, delete the content and write a header
+    if(last_existing_time == datetime.min):
+        f.truncate()
+        writer.writerow(['date', 'id', 'start_station', 'start_time', 'end_station', 'end_time', 'price'])
     writer.writerows(output)
