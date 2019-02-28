fe5d7c7874cbf749008e2fe6ef7f9e5ab792a4ff
Bernhard <berni98k@gmail.com>
2019-02-28 12:55:25 +0100

    use the official API instead of citybik.es

--- get_nearby_stations.py
+++ get_nearby_stations.py
@@ -1,10 +1,12 @@
 from operator import itemgetter
 
 import requests
+import xmltodict
 from geopy import distance
 
 
 def count_to_emoji(i):
+    i = int(i)
     if i == 0:
         return "❌"
     if i <= 5:
@@ -22,18 +24,18 @@ def get_nearby_stations(update, context):
     if update.message:
         loc = update.message.location
 
-        r = requests.get('https://api.citybik.es/v2/networks/citybike-wien')
-        stations = r.json()['network']['stations']
+        r = requests.get('http://dynamisch.citybikewien.at/citybike_xml.php')
+        stations = xmltodict.parse(r.content)['stations']['station']
         for s in stations:
             s['distance'] = distance.distance((loc.latitude, loc.longitude), (s['latitude'], s['longitude'])).meters
         near_stations = sorted(stations, key=itemgetter('distance'))[:3]
 
         station_text = "[{name}](http://maps.google.com/maps?q={latitude},{longitude}) ({distance_text})\n" \
                        "{free_bikes_emoji} Bikes (*{free_bikes}*)\n" \
-                       "{empty_slots_emoji} Slots (*{empty_slots}*)"
+                       "{free_boxes_emoji} Slots (*{free_boxes}*)"
         station_texts = [station_text.format(**s,
                                              free_bikes_emoji=count_to_emoji(s['free_bikes']),
-                                             empty_slots_emoji=count_to_emoji(s['empty_slots']),
+                                             free_boxes_emoji=count_to_emoji(s['free_boxes']),
                                              distance_text=format_meters(s['distance'])
                                              ) for s in near_stations]
 
