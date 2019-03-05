0f3a7e4ca7d43b3d378739c49dc6bff8e799e6b1
Bernhard <berni98k@gmail.com>
2019-03-05 12:23:31 +0100

    Use callbacks instead of returns between stationBot and stationService

--- stationBot.py
+++ stationBot.py
@@ -5,44 +5,43 @@ import stationService
 from utils import Location
 
 
-def reply(update, context, replies):
-    if isinstance(replies, str):
-        replies = [replies]
-    for r in replies:
-        context.bot.send_message(chat_id=update.message.chat_id, text=r, parse_mode="Markdown",
+def reply_function(update, context):
+    def reply(text):
+        context.bot.send_message(chat_id=update.message.chat_id, text=text, parse_mode="Markdown",
                                  disable_web_page_preview=True, reply_markup=ReplyKeyboardRemove())
+    return reply
 
 
 def get_nearby_stations(update, context):
     if update.message:
-        reply(update, context, stationService.get_nearby_stations_message(
-            get_location_from_update(update),
-            update.message.chat_id
-        ))
-        return -1
+        stationService.get_nearby_stations_message(
+            update.message.chat_id,
+            reply_function(update, context),
+            get_location_from_update(update)
+        )
+    return -1
 
 
 def set_home_init(update, context):
-    reply(update, context,
-          "Send a location to set its nearest station as home.\nUse /cancel to cancel, or /delete to remove it")
+    reply_function(update, context)("Send a location to set its nearest station as home.\nUse /cancel to cancel, "
+                                    "or /delete to remove it")
     return "SETHOME"
 
 
 def set_home(update, context):
     if not update.message:
-        return invald_message(update, context)
+        return invalid_message(update, context)
 
-    replies = stationService.set_home(get_location_from_update(update), update.message.chat_id)
-    reply(update, context, replies)
+    stationService.set_home(update.message.chat_id, reply_function(update, context), get_location_from_update(update))
     return -1
 
 
-def invald_message(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Please send a location next to your Home Station")
+def invalid_message(update, context):
+    reply_function(update, context)("Please send a location next to your Home Station")
 
 
 def delete_home(update, context):
-    reply(update, context, stationService.delete_home(update.message.chat_id))
+    stationService.delete_home(update.message.chat_id, reply_function(update, context))
     return -1
 
 
@@ -55,4 +54,4 @@ location_info_handler = ConversationHandler(entry_points=[MessageHandler(Filters
                                             states={"SETHOME": [CommandHandler("cancel", lambda x, y: -1),
                                                                 CommandHandler("delete", delete_home),
                                                                 MessageHandler(Filters.location, set_home)]},
-                                            fallbacks=[MessageHandler(None, invald_message)])
+                                            fallbacks=[MessageHandler(None, invalid_message)])
--- stationService.py
+++ stationService.py
@@ -2,30 +2,26 @@ import persistance
 import stationAPI
 
 
-def get_nearby_stations_message(loc, user_id):
-    replies = []
-
+def get_nearby_stations_message(user_id, reply, loc):
     near_stations = stationAPI.get_nearest_stations(loc, 3)
     station_texts = [s.formatted_string() for s in near_stations]
 
-    replies.append("\n\n".join(station_texts))
+    reply("\n\n".join(station_texts))
 
     if persistance.get_setting(user_id, 'home_station') is None:
-        replies.append("No home station set.\nUse /sethome to set it")
+        reply("No home station set.\nUse /sethome to set it")
     else:
         home_station = stationAPI.get_station_by_id(persistance.get_setting(user_id, 'home_station'))
         home_station.calculate_distance(loc)
-        replies.append("*Your Home Station:*\n" + home_station.formatted_string())
-
-    return replies
+        reply("*Your Home Station:*\n" + home_station.formatted_string())
 
 
-def set_home(loc, user_id):
+def set_home(user_id, reply, loc):
     home_station = stationAPI.get_nearest_stations(loc, 1)[0]
     persistance.set_setting(user_id, 'home_station', home_station.id)
-    return "*Home set to:*\n" + home_station.formatted_string()
+    reply("*Home set to:*\n" + home_station.formatted_string())
 
 
-def delete_home(user_id):
+def delete_home(user_id, reply):
     persistance.set_setting(user_id, 'home_station', None)
-    return "Your Home Station was deleted!\nUse /sethome to set it again"
+    reply("Your Home Station was deleted!\nUse /sethome to set it again")
