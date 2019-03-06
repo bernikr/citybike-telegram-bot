3bef9265f5da686ed78bdc9223f0af3ea4e6c525
Bernhard <berni98k@gmail.com>
2019-03-06 19:13:20 +0100

    remove unused code

--- utils.py
+++ utils.py
@@ -6,13 +6,6 @@ from telegram import ReplyKeyboardRemove
 logger = logging.getLogger(__name__)
 
 
-def normalize_umlauts(text):
-    chars = {u'ö': 'oe', u'ä': 'ae', u'ü': 'ue', u'ß': 'ss'}
-    for char in chars:
-        text = text.replace(char, chars[char])
-    return text
-
-
 def reply_function(update, context):
     def reply(text):
         context.bot.send_message(chat_id=update.message.chat_id, text=text, parse_mode="Markdown",
