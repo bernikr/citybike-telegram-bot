e8335495f8933d9f597190e9c9eb87192cf255ee
Bernhard <berni98k@gmail.com>
2019-04-05 15:59:57 +0200

    restart the service after deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
 scp build/libs/citybikes.jar citybikebot@euclid.srv.kralofsky.com:/var/citybikes/citybikes.jar
+ssh citybikebot@euclid.srv.kralofsky.com 'sudo systemctl restart citybikes'
