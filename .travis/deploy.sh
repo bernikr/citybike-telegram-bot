6ed7688e5cdf9da149de6ae8ce459874bf841708
Bernhard <berni98k@gmail.com>
2019-04-05 15:20:06 +0200

    fix bug in deployment script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-scp /build/libs/citybikes.jar citybikebot@euclid.srv.kralofsky.com:/var/citybikes/citybikes.jar
+scp build/libs/citybikes.jar citybikebot@euclid.srv.kralofsky.com:/var/citybikes/citybikes.jar
