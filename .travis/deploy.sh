91d5a31fe14725fb951b84b3685af80daa351f33
Bernhard <berni98k@gmail.com>
2019-04-05 14:24:26 +0200

    fix deployment path

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 git config --global push.default matching
-git remote add deploy ssh://citybikebot@euclid.srv.kralofsky.com/citybike-telegram-bot
+git remote add deploy citybikebot@euclid.srv.kralofsky.com:citybike-telegram-bot
 git push deploy master
