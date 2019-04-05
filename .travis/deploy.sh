a8c5d48ba06384e88ec0e6359d3ef55f34f1e849
Bernhard <berni98k@gmail.com>
2019-04-05 15:17:16 +0200

    deploy via jar

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 set -e
-git config --global push.default simple
-git remote add deploy citybikebot@euclid.srv.kralofsky.com:citybike-telegram-bot
-git push deploy master
+scp /build/libs/citybikes.jar citybikebot@euclid.srv.kralofsky.com:/var/citybikes/citybikes.jar
