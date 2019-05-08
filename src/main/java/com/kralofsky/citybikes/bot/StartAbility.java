1692cd23a18f8e7bd57c62eefbb1432031c2d7a9
Bernhard Kralofsky <bernhard@kralofsky.com>
2019-05-08 15:52:27 +0200

    reply to the start message with a welcome message

---
+++ src/main/java/com/kralofsky/citybikes/bot/StartAbility.java
@@ -0,0 +1,31 @@
+package com.kralofsky.citybikes.bot;
+
+import com.kralofsky.citybikes.bot.util.ExternalAbility;
+import org.springframework.stereotype.Component;
+import org.telegram.abilitybots.api.objects.MessageContext;
+
+import static org.telegram.abilitybots.api.objects.Locality.USER;
+import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
+
+@Component
+public class StartAbility extends ExternalAbility {
+    private final String message = "Just send a location to get information on the Citybike Stations around you. Use /sethome to set your favourite station that gets displayed every time regardless of your location.\n" +
+            "\n" +
+            "THIS BOT IS UNOFFICIAL:\n" +
+            "Citybike and the GEWISTA Werbegesellschaft m.b.H. are NOT associated in any way with the operator of this bot, are NOT responsible for the content of this bot and take NO responsibility or liability concerning accuracy or timeliness of the data provided.";
+
+    @Override
+    protected AbilityOptions getOptions() {
+        return AbilityOptions.builder()
+                .name("start")
+                .input(0)
+                .locality(USER)
+                .privacy(PUBLIC)
+                .build();
+    }
+
+    @Override
+    protected void action(MessageContext ctx) {
+        silent.send(message, ctx.chatId());
+    }
+}
