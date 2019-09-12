4ab406ab20429adbc004f33a67b0544e2a5c4000
Bernhard Kralofsky <bernhard@kralofsky.com>
2019-09-12 22:36:28 +0300

    simplify the App class

--- src/main/java/com/kralofsky/citybikes/CitybikeTelegramBotApp.kt
+++ src/main/java/com/kralofsky/citybikes/CitybikeTelegramBotApp.kt
@@ -7,14 +7,10 @@ import org.telegram.telegrambots.ApiContextInitializer
 import java.util.Locale
 
 @SpringBootApplication
-open class CitybikeTelegramBotApp {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            Locale.setDefault(Locale.US)
-            ApiContextInitializer.init()
-            runApplication<CitybikeTelegramBotApp>(*args)
-        }
-    }
-}
+class CitybikeTelegramBotApp
 
+fun main(args: Array<String>) {
+    Locale.setDefault(Locale.US)
+    ApiContextInitializer.init()
+    runApplication<CitybikeTelegramBotApp>(*args)
+}
