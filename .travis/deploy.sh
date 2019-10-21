75666fd3c0cc4ded973a979943c1eb6532863ff7
Bernhard Kralofsky <bernhard@kralofsky.com>
2019-10-21 20:51:20 +0300

    remove travis since the deployment was transferred to heroku

--- .travis/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-scp build/libs/citybikes.jar citybikebot@euclid.srv.kralofsky.com:/var/citybikes/citybikes.jar
-ssh citybikebot@euclid.srv.kralofsky.com 'sudo systemctl restart citybikes'
